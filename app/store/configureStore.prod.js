b619703d7e2c24658710d0e9e496be65b8487c1c
blade-running-man <oleksandr.slukovskyi@gmail.com>
2019-05-28 00:29:40 +0300

    first commit

---
+++ app/store/configureStore.prod.js
@@ -0,0 +1,20 @@
+// @flow
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { createHashHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+import createRootReducer from '../reducers';
+
+const history = createHashHistory();
+const rootReducer = createRootReducer(history);
+const router = routerMiddleware(history);
+const enhancer = applyMiddleware(thunk, router);
+
+function configureStore() {
+  return createStore<*, counterStateType, *>(
+    rootReducer,
+    enhancer
+  );
+}
+
+export default { configureStore, history };
